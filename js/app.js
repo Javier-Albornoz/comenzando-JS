478b508f66bc149de377adda01c0d224cc650c52
JavierAlbornoz <javix781@gmail.com>
2021-09-15 20:47:25 -0300

    sume parseo

--- js/app.js
+++ js/app.js
@@ -3,3 +3,6 @@ alert('hola como estas?');
 let nombre = prompt('cual es tu nombre?');
 let apellido = prompt('y tu apellido?');
 alert('Espero que tengas un Muy buen día! '+ nombre + ' ' + apellido)
+const edad = Number( prompt('que edad tienes?'));
+const nuevaEdad= edad + 3;
+alert('en tres años tendras ' +nuevaEdad);
