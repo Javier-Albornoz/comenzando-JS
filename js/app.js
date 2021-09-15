9bb5549671821f8283ed1f7b8ce019ceafc868d1
JavierAlbornoz <javix781@gmail.com>
2021-09-14 22:20:24 -0300

    comenzando Js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log('Primer desafio');
+alert('hola como estas?');
+let nombre = prompt('cual es tu nombre?');
+let apellido = prompt('y tu apellido?');
+alert('Espero que tengas un Muy buen día! '+ nombre + ' ' + apellido)
